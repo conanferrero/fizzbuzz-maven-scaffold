f2c9fc630b5343799af126f0a6557ee461960e8f
conanferrero <conanferrero@163.com>
2019-12-28 13:44:46 +0800

    refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -5,27 +5,43 @@ public class FizzBuzz {
         this.value = i;
     }
 
-    @Override
-    public String toString() {
+
+    public String toString1() throws IllegalAccessException{
+        if (this.value <= 0) {
+            throw new IllegalAccessException("Invalid Input");
+        }
         String s1 = returnFizz();
         String s2 = returnBuzz();
-        String result = s1 + s2;
-        if (!result.isEmpty()) return result;
+        String s3 = returnWhizz();
+        String result = s1 + s2 + s3;
+        if (!result.isEmpty()) return  result;
         return String.valueOf(this.value);
     }
 
-    private boolean isDivBy(int i) {
-        if (this.value % i == 0) return true;
-        return false;
+    private boolean isDivBy(int i)
+    {
+        if (this.value % i == 0)
+        {
+            return true;
+        }
+        return  false;
     }
 
-    private String returnFizz() {
+    private String returnFizz()
+    {
         if (isDivBy(3)) return "Fizz";
         return "";
     }
 
-    private String returnBuzz() {
+    private String returnBuzz()
+    {
         if (isDivBy(5)) return "Buzz";
+        return "";
+    }
+
+    private String returnWhizz()
+    {
+        if (isDivBy(7)) return "Whizz";
         return "";
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,35 +5,45 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
-   /* @Test
+    /*@Test
     void return_1_given_1() {
-        FizzBuzz item =  new FizzBuzz(1);
+        FizzBuzz item = new FizzBuzz(1);
         assertEquals("1",item.toString());
     }@Test
     void return_Fizz_given_3() {
-        FizzBuzz item =  new FizzBuzz(3);
+        FizzBuzz item = new FizzBuzz(3);
         assertEquals("Fizz",item.toString());
     }@Test
     void return_Buzz_given_5() {
-        FizzBuzz item =  new FizzBuzz(5);
+        FizzBuzz item = new FizzBuzz(5);
         assertEquals("Buzz",item.toString());
     }@Test
     void return_FizzBuzz_given_15() {
-        FizzBuzz item =  new FizzBuzz(15);
+        FizzBuzz item = new FizzBuzz(15);
         assertEquals("FizzBuzz",item.toString());
     }*/
 
+
     @ParameterizedTest(name = "return {1} given {0}")
-    @CsvSource(
-            {
-                    "1,1",
-                    "3,Fizz",
-                    "5,Buzz",
-                    "15,FizzBuzz",
-            }
-    )
-    void testFizzBuzz(int input, String expected) {
+    @CsvSource({
+            "1,1",
+            "3, Fizz",
+            "5, Buzz",
+            "7, Whizz",
+            "15, FizzBuzz",
+            "21, FizzWhizz",
+            "35, BuzzWhizz",
+            "105, FizzBuzzWhizz",
+            "0, Invalid Input",
+    })
+    void testFizzBuzz(int input, String expected)
+    {
         FizzBuzz item = new FizzBuzz(input);
-        assertEquals(expected, item.toString());
+        try {
+            assertEquals(expected,item.toString1());
+        } catch (IllegalAccessException e) {
+            assertEquals(expected,e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
